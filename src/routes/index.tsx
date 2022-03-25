bec1a56a69ef98732ec902499e9b1b1f71f90748
wiuribecomodisimos <wiuribe@comodisimos.com>
2022-03-24 21:11:29 -0500

    📦 NEW: base

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,19 @@
-import About from '@/pages/About';
 import { RouteObject } from 'react-router-dom';
-import Layout from '@/layout/Main';
+import MainLayout from '@/layout/MainLayout';
+import About from '@/pages/About';
 import Home from '@/pages/Home';
 import Error404 from '@/pages/Error404';
 
-export default [
+const routes: RouteObject[] = [
   {
     path: "/",
-    element: <Layout />,
+    element: <MainLayout />,
     children: [
       { index: true, element: <Home /> },
       { path: "about", element: <About /> },
     ],
   },
   { path: "*", element: <Error404 /> },
-] as RouteObject[];
+]
+
+export default routes;
